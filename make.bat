190922eba620507ddc739296a4f17df66ef687db
Serge NKS <57449566+senaki@users.noreply.github.com>
2021-01-26 15:29:42 +0100

    - Add make.bat - Add a test file test.dat

---
+++ make.bat
@@ -0,0 +1,28 @@
+@echo off
+REM:Makefie
+set src=matrix.c jacobi.c main.c
+set obj=matrix.o jacobi.o main.o
+set opt1= -static -std=gnu11 -lm -W -Wall
+set gcc="c:\mingw\bin\gcc.exe"
+set prog=rotjacobi.exe
+if "%1"=="clean" goto clean
+if "%1"=="object" goto create_obj
+if "%1"=="exec" goto create_exe
+if "%1"=="all" goto all
+REM: Compiling the code
+:create_obj
+echo Compiling %src%
+gcc  -c %src% %opt1%
+exit
+:create_exe
+echo Assembling %prog%
+gcc %obj% -o %prog%
+exit
+:all
+echo Compiling %src%
+gcc %src% %opt1% -o %prog%
+exit
+:clean
+echo Deleting of %obj%
+del %obj%
+exit
