18d8ce2b1702fd29108efbfd8e19b8f42ad72fa9
Serge NKS <57449566+senaki@users.noreply.github.com>
2021-01-27 02:39:07 +0100

    Prototype modification

--- inclusions.h
+++ inclusions.h
@@ -16,8 +16,8 @@ extern int mat_write( unsigned int n,unsigned int m, double *a, char *file_name,
 extern unsigned int jacobi(const double *m, unsigned int n, double *spectre);
 extern int IsSym( unsigned int nrow, unsigned int ncol, double *in);//Check if the matrix is symmetric
 extern int IsOrtho(unsigned int n, double *in);
-extern void transpose(unsigned int nrow, unsigned int ncol, double *A, double *out);
+extern void transpose(unsigned int nrow, unsigned int ncol, const double *A, double *out);
 extern void Cross(unsigned int a_nrow, unsigned int b_nrow, unsigned int a_ncol,unsigned int b_ncol, double *A, double *B, double *C);//matricial product
-extern void mat_sum(unsigned int n, double *a, double *b, double *out); //matrix sum
+extern void mat_sum(unsigned int n, const double *a, const double *b, double *out); //matrix sum
 extern double **mat_eye(unsigned int dim) ;
 #endif //INCLUSIONS_H_INCLUDED
