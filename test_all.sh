dbf75d6cc01b533408e6c050a3490dd5e6ddfe23
Serge NKS <57449566+senaki@users.noreply.github.com>
2021-02-22 00:27:39 +0100

    Update

--- test_all.sh
+++ test_all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 inPath=$(readlink -f $1);
-for file in $(dir $1)
+for fich in $(ls $1)
 do
-	#  echo ""
-	 ./rotjacobi "$(readlink -f ${file})" $2 ;
-done
+	 # echo "$inPath/${file}"
+	 ./rotjacobi "$1/${fich}" $2 ;
+done | sed -n -r 's/^.*[[:blank:]]Dim=([[:digit:]]+)[[:blank:]]Rotations=([[:digit:]]+)[[:blank:]]Duration=([[:digit:]]+) ms/\1\t\2\t\3/g;p' > $2/test_all.log
 echo -e "Congratulations, it's all done !"
 exit 0 ;
